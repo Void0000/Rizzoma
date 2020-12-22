db81e57bc91c0302b2171534cb9086438fc34d92
unknown <void0000@mail.ru>
2020-12-22 06:33:34 +0300

    add juqery

--- PUG/src/scripts/index.ts
+++ PUG/src/scripts/index.ts
@@ -6,3 +6,9 @@ import './../styles/main.scss';
 //$('p').click(function(){
   //alert('he paragraph was clicked.');
 //});
+
+//jQuery(document).ready(function($){
+
+////	$('.date').mask('00/00/0000');
+
+//});
