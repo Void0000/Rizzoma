2041606c49284dace62891c0dd24eed1f67022f3
unknown <void0000@mail.ru>
2020-12-23 03:34:50 +0300

    made checkbox and radio buttons

--- PUG/src/scripts/index.ts
+++ PUG/src/scripts/index.ts
@@ -1,20 +1,33 @@
 import './../styles/main.scss';
-
-
 //import { showMenu } from './menu';
-
 //showMenu();
 
-//$('p').click(function(){
-  //alert('he paragraph was clicked.');
-//});
+
+
 
    jQuery(document).ready(function($){
+       $('[name=date]').mask('99/99/9999',{placeholder:'ДД.ММ.ГГГГ'} );   
+      });
 
-       $('#dd').inputmask('mmddyyyy');
-       
-	
+    jQuery(document).ready(function(){
+      $('.Smoke').click(function () {
+          $('.containerSmoke').toggleClass('containerChecked')});
+      $('.Pets').click(function () {
+          $('.containerPets').toggleClass('containerChecked')});
+      $('.Guests').click(function () {
+          $('.containerGuests').toggleClass('containerChecked');
+      });
+      });
 
-  });
+      jQuery(document).ready(function(){
+        $('.Man').click(function () {
+            $('.containerRadio.M').toggleClass('containerRadioChecked')});
+        $('.Women').click(function () {
+            $('.containerRadio.Women').toggleClass('containerRadioChecked');
+        });
+        });
+      
 
-  
+
+
+   
